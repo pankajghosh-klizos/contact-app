27657f5067dff4e6aa086b701c664eda20a4319b
Pankaj Ghosh <pankaj.ghosh@klizos.com>
2024-12-17 11:48:31 +0530

    initial commit

---
+++ src/routes/root.jsx
@@ -0,0 +1,70 @@
+import {
+  Outlet,
+  useLoaderData,
+  Form,
+  NavLink,
+  useNavigation,
+} from "react-router-dom";
+import { getContacts, createContact } from "../contacts.js";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
+
+export default function Root() {
+  const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <div id="sidebar">
+        <div>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
+      </div>
+      <div
+        id="detail"
+        className={navigation.state === "loading" ? "loading" : ""}
+      >
+        <Outlet />
+      </div>
+    </>
+  );
+}
